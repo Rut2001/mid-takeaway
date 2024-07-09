60cca0aaf09ff749f62e004492e82dfce1686b2d
Rut patel <rutpatel359@gmail.com>
2024-07-09 08:52:08 -0700

    Initial Commit

---
+++ mid takeaway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mid takeaway
+//
+//  Created by user240207 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mid takeawayUITests/mid_takeawayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mid_takeawayUITestsLaunchTests.swift
+//  mid takeawayUITests
+//
+//  Created by user240207 on 7/9/24.
+//
+
+import XCTest
+
+final class mid_takeawayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
